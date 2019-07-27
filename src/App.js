a70d3fdbce79ea732604a2d0caf1c8da271d9073
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-07-27 17:09:03 -0300

    banner component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './css/style.css';
 import Header from './components/header'
+import Banner from './components/banner';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Banner/>
       </div>
     );
   }
---
+++ src/components/banner/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import { Container } from 'semantic-ui-react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import banner from '../../img/banner.png';
+
+class Banner extends React.Component {
+    render() {
+      var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      return (
+        <Slider {...settings}>
+          <div>
+            <img src={banner} alt="banner"/>
+          </div>
+          <div>
+            <img src={banner} alt="banner"/>
+          </div>
+          <div>
+            <img src={banner} alt="banner"/>
+          </div>
+          <div>
+            <img src={banner} alt="banner"/>
+          </div>
+        </Slider>
+      );
+    }
+  }
+export default Banner;
